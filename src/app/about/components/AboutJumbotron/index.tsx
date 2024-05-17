df53fb22943130761b042f7d8e83c785dc3dda7b
akaslayer <hendrysurijanto123@gmail.com>
2024-05-17 14:35:50 +0700

    Feat(fix): fix all the size problem in hero and jumbotron

--- src/app/about/components/AboutJumbotron/index.tsx
+++ src/app/about/components/AboutJumbotron/index.tsx
@@ -1,7 +1,19 @@
+'use client'
 import Jumbotron from '@/components/Jumbotron'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 const AboutJumbotron = () => {
+  const [useSmallImage, setUseSmallImage] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setUseSmallImage(window.innerWidth <= 400)
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => removeEventListener("resize", handleResize)
+  })
   const leftContent = (
     <div className="flex h-full px-5 py-20  bg-blue-700 relative items-center justify-end align-bottom md:py-0 md:mt-0 md:px-20 ">
       <span className='shape3 p-20 bg-black absolute right-0 bottom-0 h-full rotate-180 opacity-35 z-0'></span>
@@ -14,15 +26,25 @@ const AboutJumbotron = () => {
   );
   const rightContent = (
     <div className="w-full h-full">
-      <Image
-        width={640}
-        height={360}
-        src={'https://res.cloudinary.com/dv9bbdl6i/image/upload/c_fill,w_640,h_360/v1715927723/jumbotron-about_iglyca.webp'}
+      {useSmallImage ? <Image
+        width={400}
+        height={225}
+        src={'https://res.cloudinary.com/dv9bbdl6i/image/upload/c_fill,w_400,h_225/v1715927723/jumbotron-about_iglyca.webp'}
         layout="responsive"
         alt='aboutJumbotron'
         className="w-full h-full object-cover"
         loading="eager"
-      />
+      /> :
+        <Image
+          width={640}
+          height={360}
+          src={'https://res.cloudinary.com/dv9bbdl6i/image/upload/c_fill,w_640,h_360/v1715927723/jumbotron-about_iglyca.webp'}
+          layout="responsive"
+          alt='aboutJumbotron'
+          className="w-full h-full object-cover"
+          loading="eager"
+        />}
+
     </div>
   );
   return (
